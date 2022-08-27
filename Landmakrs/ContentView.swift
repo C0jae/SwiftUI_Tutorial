40871e0d6638c14b7a7eccc645367321523810c0
Youngjae Choi <cdw0807@naver.com>
2022-08-27 18:08:33 +0900

    Initial Commit

---
+++ Landmakrs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmakrs
+//
+//  Created by 최영재 on 2022/08/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmakrs/LandmakrsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmakrsApp.swift
+//  Landmakrs
+//
+//  Created by 최영재 on 2022/08/27.
+//
+
+import SwiftUI
+
+@main
+struct LandmakrsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
